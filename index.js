0c40d0f1fc5c94593d35508e973c76c63ae1d0e7
onthesub <aaron.schieffer@gmail.com>
2024-11-26 22:19:39 -0600

    use cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./router')
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use('/', router)
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
